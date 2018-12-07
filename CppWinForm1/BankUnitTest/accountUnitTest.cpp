0fab9a6f5d0c99505ad0d47c9915fbf1ad0f332a
mariam2019 <mariammaher2019@gmail.com>
2018-12-07 23:02:56 +0200

    added some new tests

--- CppWinForm1/BankUnitTest/accountUnitTest.cpp
+++ CppWinForm1/BankUnitTest/accountUnitTest.cpp
@@ -1,7 +1,10 @@
+
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include"../Project1/account.h"
 #include"accountReaderTEST.h"
+#include"accountPROXY.h"
+
 #include<string>
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -23,6 +26,70 @@ namespace BankUnitTest
 			Assert::AreEqual("mariam", accountUNDERTEST.getName());
 			
 			
+		}
+
+
+		TEST_METHOD(retdepositTEST)
+		{
+			Logger::WriteMessage("Return Deposit Test");
+
+			accountReaderTEST* accntReaderTest = new accountReaderTEST();
+			account accountUNDERTEST(accntReaderTest);
+
+			accntReaderTest->setTESTdeposit(50);
+			accountUNDERTEST.create_account();
+
+			accountPROXY * accountUNDERTEST_PROXY = reinterpret_cast<accountPROXY *> ( & accountUNDERTEST);
+
+			Assert::AreEqual( accountUNDERTEST_PROXY->deposit ,accountUNDERTEST.retdeposit(), L"Returned deposit and expected deposit are not equal");
+		}
+
+		TEST_METHOD(drawTEST)
+		{
+			Logger::WriteMessage("Draw From Deposit Test");
+
+			int intialDeposit = 500;
+			int drawAmnt = 50;
+			int negDrawAmnt = -50;
+
+			accountReaderTEST* accntReaderTest = new accountReaderTEST();
+			account accountUNDERTEST(accntReaderTest);
+			accountPROXY * accountUNDERTEST_PROXY = reinterpret_cast<accountPROXY *> (&accountUNDERTEST);
+
+			accntReaderTest->setTESTdeposit(intialDeposit);
+			accountUNDERTEST.create_account();
+			accountUNDERTEST.draw(drawAmnt);
+
+			Assert::AreEqual(intialDeposit-drawAmnt, accountUNDERTEST_PROXY->deposit,L"Drawn amount was not subtracted correctly from deposit");
+		
+			bool exceptionThrown = false;
+			try
+			{
+				accountUNDERTEST.draw(negDrawAmnt);
+			}
+			catch(exception e)
+			{
+				string expectedEx = "Amount to be drawn can not be negative";
+				exceptionThrown = true;
+				string excDesc = e.what();
+				Assert::AreEqual(expectedEx,excDesc ,L"Exception message wasn't as expected");
+			}
+			Assert::IsTrue(exceptionThrown, L"Exception wasn't thrown when input was negative");
+
+			exceptionThrown = false;
+			try
+			{
+				accountUNDERTEST.draw(intialDeposit - drawAmnt+1);
+			}
+			catch (exception e)
+			{
+				string expectedEx = "Amount to be drawn can not be bigger than deposit";
+				exceptionThrown = true;
+				string excDesc = e.what();
+				Assert::AreEqual(expectedEx, excDesc, L"Exception message wasn't as expected");
+			}
+			Assert::IsTrue(exceptionThrown, L"Exception wasn't thrown when input was bigger than deposit");
+
 		}
 
 	};
